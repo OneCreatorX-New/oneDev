78cc9bb3dca8311f1e77c0f20395a953429fdc82
OneCreatorX-New <cuentapanama19@gmail.com>
2024-06-23 03:34:12 -0300

    Update 17763012877.lua

--- 17763012877.lua
+++ 17763012877.lua
@@ -23,9 +23,9 @@ setreadonly(mt, false)
 mt.__index = function(instance, index)
     if active then
         if instance == damage and index == "Value" then
-            return 100000
+            return 10000000
         elseif instance == multiplier and index == "Value" then
-            return 1000
+            return 10000
         end
     end
     return old_index(instance, index)
@@ -33,6 +33,7 @@ end
 
 setreadonly(mt, true)
 
+local tp = false
 UL:AddTBtn(cfrm, "Inject DMG", false, function() 
     active = not active 
 end)
@@ -69,17 +70,28 @@ UL:AddTBtn(cfrm, "Auto Hit Block", false, function()
         for _, obj in ipairs(area:GetDescendants()) do
             if obj:IsA("ClickDetector") then
                 obj.MaxActivationDistance = 10000
+spawn(function()
+pcall(function()
+if tp and obj.Parent.Transparency == 0 and not obj.Parent.BillboardGui.Frame.HealthText.Text :find("Cooldown") then
+           p.Character.PrimaryPart.CFrame = obj.Parent.CFrame + Vector3.new(0, 2, 0)
+
+end
+end)
+end)
                 fireclickdetector(obj)
 wait()
             end
         end
     end
 end)
+UL:AddTBtn(cfrm, "TP Block", false, function() 
+tp = not tp
+end)
 
 UL:AddText(crFrm, "By Script: OneCreatorX ")
 UL:AddText(crFrm, "Create Script: 20/06/24 ")
-UL:AddText(crFrm, "Update Script: 23/06/--")
-UL:AddText(crFrm, "Script Version: 0.3")
+UL:AddText(crFrm, "Update Script: 23/06/24")
+UL:AddText(crFrm, "Script Version: 0.4")
 UL:AddBtn(crFrm, "Copy link YouTube", function() setclipboard("https://youtube.com/@onecreatorx") end)
 UL:AddBtn(crFrm, "Copy link Discord", function() setclipboard("https://discord.com/invite/UNJpdJx7c4") end)
 
