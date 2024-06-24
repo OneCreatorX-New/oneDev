28214eb9be55cf5fd751ffa37b2e41aa55593e1b
OneCreatorX-New <cuentapanama19@gmail.com>
2024-06-24 16:14:02 -0300

    Update 17841110913.lua

--- 17841110913.lua
+++ 17841110913.lua
@@ -1,51 +1,84 @@
 local UL = loadstring(game:HttpGet("https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/UIs/MyLibrery.lua"))()
 
-local function cleanGameName(name)
-    name = name:gsub("%b[]", "")
-    name = name:match("^[^:]*")
-    return name:match("^%s*(.-)%s*$")
-end
-
-local gameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
-gameName = cleanGameName(gameName)
+local gameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name:gsub("%b[]", ""):match("^[^:]*"):match("^%s*(.-)%s*$")
 
 local p = game.Players.LocalPlayer
 local sg = UL:CrSG("Default")
 local frm, cfrm, crFrm = UL:CrFrm(sg, gameName)
 
-local autoCollectEnabled = false
-
-local function autoCollectCandies()
-    while autoCollectEnabled do
-        for _, obj in ipairs(workspace.Candies:GetChildren()) do
-            if obj:IsA("BasePart") and obj:FindFirstChild("TouchInterest") then
-                local plr = game.Players.LocalPlayer
-                firetouchinterest(plr.Character.HumanoidRootPart, obj, 0)
-                wait()
-                firetouchinterest(plr.Character.HumanoidRootPart, obj, 1)
-                wait()
-            end
-        end
-        wait(0.5)
-    end
-end
-
-UL:AddTBtn(cfrm, "Auto Candy Collect", autoCollectEnabled, function(state)
-    autoCollectEnabled = state
-    if autoCollectEnabled then
-        autoCollectCandies()
-    end
-end)
-
 UL:AddText(crFrm, "By Script: OneCreatorX ")
-UL:AddText(crFrm, "Create Script: 13/06/24 ")
+UL:AddText(crFrm, "Create Script: 29/05/24 ")
 UL:AddText(crFrm, "Update Script: --/--/--")
 UL:AddText(crFrm, "Script Version: 0.1")
-
 UL:AddBtn(crFrm, "Copy link YouTube", function() setclipboard("https://youtube.com/@onecreatorx") end)
 UL:AddBtn(crFrm, "Copy link Discord", function() setclipboard("https://discord.com/invite/UNJpdJx7c4") end)
 
 game:GetService('Players').LocalPlayer.Idled:Connect(function()
     game:GetService('VirtualUser'):CaptureController()
     game:GetService('VirtualUser'):ClickButton2(Vector2.new())
+end)
+
+local function getHorizontalDistance(p1, p2)
+    return (Vector3.new(p1.X, 0, p1.Z) - Vector3.new(p2.X, 0, p2.Z)).magnitude
+end
+
+local a = false
+local ja = true
+
+UL:AddTBtn(cfrm, "Auto Candies", false, function() 
+    a = not a
+    ja = true
+end)
+
+local function findNearestCoin()
+    local nearest, minDist = nil, math.huge
+    for _, coin in pairs(workspace:GetDescendants()) do
+        if coin:IsA("BasePart") and coin:FindFirstChild("TouchInterest") and a then
+            local dist = getHorizontalDistance(p.Character.HumanoidRootPart.Position, coin.Position)
+            if dist < minDist then
+                minDist, nearest = dist, coin
+            end
+        end
+    end
+    return nearest
+end
+
+local function calculateVelocity(targetPos, speed)
+    local playerPos = p.Character.HumanoidRootPart.Position
+    return (targetPos - playerPos).unit * speed
+end
+
+local function collectCoin(player, coin)
+    local humanoid = player.Character:WaitForChild("Humanoid")
+    local tolerance, maxDist = 2, 2
+    
+    if coin and coin.Parent and a then
+        pcall(function()
+            local playerPos, coinPos = humanoid.RootPart.Position, coin.Position
+            local dist = getHorizontalDistance(playerPos, coinPos)
+            
+            if dist <= tolerance and dist <= maxDist then
+                if dist > 2 then coin.Transparency = 1 end
+                ja = false
+                wait(0.1)
+
+                spawn(function()
+                    coin:Destroy()
+                end)
+                ja = true
+            else
+                humanoid.WalkToPoint = coinPos
+                humanoid.WalkSpeed = 17  -- Ajusta la velocidad de caminar según sea necesario
+            end
+        end)
+        wait()
+    end
+end
+
+game:GetService("RunService").RenderStepped:Connect(function()
+    local nearestCoin = findNearestCoin()
+    if nearestCoin and a and ja then
+        collectCoin(p, nearestCoin)
+    end
+    wait()
 end)
